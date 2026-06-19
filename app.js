c25b13b47b80e9b3e4d1a41f72151215339fcba6
roisx00 <roisx00@gmail.com>
2026-06-19 08:37:28 +0200

    Update handle to @engmiHQ (linked) in boost-odds copy; cache v32

--- app.js
+++ app.js
@@ -494,7 +494,7 @@ function stepBurn() {
       real time. <b>The more you burn, the higher your approval odds.</b></p>
 
     <p class="bonus-line">Boost your odds: burn it <b>live</b> on stream or in a public road and tag
-      <b>@NGMI</b> in the video. Burn it <b>fully naked</b> and you skip review entirely - instant whitelist.</p>
+      <b>@engmiHQ</b> in the video. Burn it <b>fully naked</b> and you skip review entirely - instant whitelist.</p>
 
     <div class="field">
       <label>Wallet</label>
